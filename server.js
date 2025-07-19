dec05e308f9244c0924048722abebf1a0103acb0
Olistar <130336147+Olanakelbesa@users.noreply.github.com>
2025-07-19 23:18:51 +0300

    route changed to /

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/songs", songRoutes);
+app.use("/", songRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
