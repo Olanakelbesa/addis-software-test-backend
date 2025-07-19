5408042bdb4ab9a77d79e92cc021dda137b2f525
Olistar <130336147+Olanakelbesa@users.noreply.github.com>
2025-07-20 01:56:43 +0300

    cor error fixed

--- server.js
+++ server.js
@@ -8,10 +8,15 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
+
 app.use(express.json());
 
-app.use("/", songRoutes);
+app.use("/songs", songRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
